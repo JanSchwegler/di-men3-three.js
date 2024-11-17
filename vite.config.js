582b41a251b1f31f10032e3ebc81cb541bc228fb
Jan Schwegler <schwegler.jan@gmail.com>
2024-11-17 18:03:11 +0100

    rename entrypoints

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),     // Main page entry
-        about: resolve(__dirname, 'src/01_setup/setup.html'),    // About page entry
-        page02: resolve(__dirname, 'src/02_line/index.html'),    // About page entry
+        main: resolve(__dirname, 'index.html'),
+        setup: resolve(__dirname, 'src/01_setup/setup.html'),
+        line: resolve(__dirname, 'src/02_line/index.html'),
       }
     }
   }
