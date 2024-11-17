63eb65db808d0d767874bf92a708b017c33c534e
Jan Schwegler <schwegler.jan@gmail.com>
2024-11-17 17:47:51 +0100

    change vite setup
    change folder from dev to src & change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),     // Main page entry
-        about: resolve(__dirname, 'dev/01_setup/setup.html'),    // About page entry
+        about: resolve(__dirname, 'src/01_setup/setup.html'),    // About page entry
+        page02: resolve(__dirname, 'src/02_line/index.html'),    // About page entry
       }
-    },
-    outDir: 'dist', // Optional: specify the output directory for build files
+    }
   }
 });
