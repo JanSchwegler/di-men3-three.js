ab38046c678bc8c686755198c35c895b51a9ed6d
Jan Schwegler <schwegler.jan@gmail.com>
2024-12-01 17:43:26 +0100

    cleanup line js file

--- src/02_line/main.js
+++ src/02_line/main.js
@@ -8,30 +8,6 @@ if (!WebGL.isWebGL2Available()) {
 	createLine();
 }
 
-function createBox() {
-	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-	const renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animate );
-	document.body.appendChild( renderer.domElement );
-
-	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	const material = new THREE.MeshBasicMaterial( { color: 0x999999 } );
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-
-	camera.position.z = 5;
-
-	function animate() {
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-
-		renderer.render( scene, camera );
-	}
-}
-
 function createLine() {
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -43,7 +19,7 @@ function createLine() {
 
 	const scene = new THREE.Scene();
 
-	const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+	const material = new THREE.LineBasicMaterial({ color: 0xcbcbff});
 
 	const points = [];
 	points.push( new THREE.Vector3( - 10, 0, 0 ) );
