8d8f4d96393bd242c2ff041114d2b59fd231ce8f
Jan Schwegler <schwegler.jan@gmail.com>
2025-01-17 21:10:27 +0100

    update 15 userinteraction

--- src/15_userinteraction/main.js
+++ src/15_userinteraction/main.js
@@ -68,7 +68,7 @@ function init() {
     window.addEventListener('mouseout', clearTouchPosition);
     window.addEventListener('mouseleave', clearTouchPosition);
     // Click event
-    window.addEventListener('click', () => handleClick(intersectedObject));
+    window.addEventListener('click', handleClick);
     window.addEventListener('touchstart', handleTouchStart);
     window.addEventListener('touchend', handleTouchEnd);
 }
@@ -128,14 +128,14 @@ function handleTouchStart(event) {
 function handleTouchEnd() {
     const touchEndTime = Date.now();
     if (touchEndTime - touchStartTime < 200 && Math.abs(touchStartPosition.x - touchPosition.x) < 0.01 && Math.abs(touchStartPosition.y - touchPosition.y) < 0.01) {
-        handleClick(intersectedObject);
+        handleClick();
     }
     clearTouchPosition();
 }
 
-function handleClick(object) {
-    if (object) {
-        switch (object.name) {
+function handleClick() {
+    if (intersectedObject) {
+        switch (intersectedObject.name) {
             case 'bunny002_1':
                 changeHTMLContent('Bunny Body');
                 break;
